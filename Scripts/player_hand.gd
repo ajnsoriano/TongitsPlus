ff1dd4f61641d007347da2c7bebe75ff9898f170
ajnsoriano <91655647+ajnsoriano@users.noreply.github.com>
2025-11-06 01:15:05 -0800

    Added deck functionality

--- Scripts/player_hand.gd
+++ Scripts/player_hand.gd
@@ -1,47 +1,44 @@
 extends Node2D
 
-const HAND_COUNT = 10
-const CARD_SCENE_PATH = "res://Scenes/card.tscn"
+
+
 const CARD_WIDTH = 60
 # HAND Y POSITION is 890 in the video
 const HAND_Y_POSITION = 200
+const DEFAULT_CARD_MOVE_SPEED = 0.1
+
 var player_hand = []
 var center_screen_x
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	center_screen_x = $"../Camera2D".get_screen_center_position().x
-	var card_scene = preload(CARD_SCENE_PATH)
-	for i in range(HAND_COUNT):
-		var new_card = card_scene.instantiate()
-		$"../CardManager".add_child(new_card)
-		new_card.name = "Card"
-		add_card_to_hand(new_card)
 	
-func add_card_to_hand(card):
+	
+func add_card_to_hand(card, speed):
 	if card not in player_hand:
 		player_hand.insert(0, card)
-		update_hand_positions()
+		update_hand_positions(speed)
 	else:
-		animate_card_to_position(card, card.hand_position)
-func update_hand_positions():
+		animate_card_to_position(card, card.hand_position, DEFAULT_CARD_MOVE_SPEED)
+		
+func update_hand_positions(speed):
 	for i in range(player_hand.size()):
 		# Get new card position based on index
 		var new_position = Vector2(calculate_card_position(i), HAND_Y_POSITION)
 		var card = player_hand[i]
 		card.hand_position = new_position
-		animate_card_to_position(card, new_position)
+		animate_card_to_position(card, new_position, speed)
 		
 func calculate_card_position(index):
 	var total_width = (player_hand.size() - 1) * CARD_WIDTH
-	@warning_ignore("integer_division")
 	var x_offset  = center_screen_x + index * CARD_WIDTH - total_width / 2.0
 	return x_offset
 
-func animate_card_to_position(card, new_position):
+func animate_card_to_position(card, new_position, speed):
 	var tween = get_tree().create_tween()
-	tween.tween_property(card, "position", new_position, 0.1)
+	tween.tween_property(card, "position", new_position, speed)
 
 func remove_card_from_hand(card):
 	if card in player_hand:
 		player_hand.erase(card)
-		update_hand_positions()
+		update_hand_positions(DEFAULT_CARD_MOVE_SPEED)
